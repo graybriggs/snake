8cf1b20196157d0801f8424eab829497d07995b7
Graham Briggs <grahambriggs@gmail.com>
2020-09-02 10:16:03 +0900

    remove cruft

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 
-#include <deque>
-
-#include <SDL.h>
 
 #include "game.h"
-void clear_screen(SDL_Renderer* renderer);
-void cleanup(SDL_Window* window, SDL_Renderer* renderer);
+
 
 int main(int argc, char* args[]) {
 
